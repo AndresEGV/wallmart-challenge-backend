b06c8f479e836ca0a2f367f6b573e44a1f308d2f
Andres <agomez.vg@gmail.com>
2022-05-28 19:35:17 -0400

    feat: add documentation with swagger

--- routes/ship.js
+++ routes/ship.js
@@ -3,6 +3,65 @@ const router = express.Router();
 const shipController = require("../controllers/shipController");
 const { check } = require("express-validator");
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Ship:
+ *      type: object
+ *      properties:
+ *        name:
+ *          type: string
+ *          description: the ship name
+ *        model:
+ *          type: string
+ *          description: the ship model
+ *        manufacter:
+ *          type: string
+ *          description: the ship manufacter
+ *        passangers:
+ *          type: number
+ *          description: the ship passangers
+ *        starShipClass:
+ *          type: string
+ *          description: the  star Ship Class
+ *        cargoCapacity:
+ *          type: number
+ *          description: the ship cargo Capacity
+ *      required:
+ *        - name
+ *        - model
+ *        - passangers
+ *        - starShipClass
+ *        - cargoCapacity
+ *      example:
+ *        name: Star-X Rebel
+ *        model: StarWing
+ *        manufacter: Andres
+ *        passangers: 4
+ *        starShipClass: Fighter
+ *        cargoCapacity: 400
+ *  */
+
+/**
+ * @swagger
+ * /api/interstellar-ships:
+ *  post:
+ *    summary: Create a new ship
+ *    tags: [Ship]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            $ref: '#/components/schemas/Ship'
+ *    responses:
+ *      200:
+ *         description: ship created successfully!
+ *
+ * */
+
 router.post(
   "/",
   [
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const conectarDb = require("./config/db");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+const path = require("path");
 require("dotenv").config();
 const app = express();
 app.use(express.json());
@@ -8,8 +11,29 @@ app.use(express.json());
 //connect to BD
 conectarDb();
 
-// Config of CORS
+//Swagger doc
+const swaggerSpec = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Node MongoDB API Starwars to create Ships",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        url: "http://localhost:4000",
+      },
+    ],
+  },
+  apis: [`${path.join(__dirname, "./routes/*.js")}`],
+};
+app.use(
+  "/api-doc",
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerJsDoc(swaggerSpec))
+);
 
+// Config of CORS
 const corsOptions = {
   origin: process.env.FRONTEND_URL,
 };
